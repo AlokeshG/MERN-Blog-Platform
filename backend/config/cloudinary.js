c8eba7b7b2b4de2dd5120fc42de9524c492b6465
Alokesh Ghosh <alokeshghosh.5555@gmail.com>
2026-07-03 13:48:09 +0530

    Check Cloudinary env

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -6,4 +6,8 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY ? "Loaded" : "Missing");
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing");
+
 module.exports = cloudinary;
